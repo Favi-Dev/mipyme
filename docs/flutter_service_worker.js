d2cdd9a84e19241aa140bd726ca45c8e6e9a7d7b
FaviDev <favi.developer@gmail.com>
2026-02-19 14:19:41 -0300

    Update GitHub Pages deployment

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf2400b92d98382d10ea1252a84e214b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "04ffd8279bda9c58b907b83d1a02af9a",
+"flutter_bootstrap.js": "c4b1bbf9843b08201db18ad5751cd578",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
